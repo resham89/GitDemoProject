73edad4ab4a53a3ed7ebefb3da7a29fe69393766
ajit1 <ajit1@DESKTOP-45HEKQS>
2023-01-29 16:31:22 +0530

    First commit

---
+++ src/Testcase1.java
@@ -0,0 +1,26 @@
+
+public class Testcase1 {
+
+public static void main(String[] args) {
+	
+	String a="HYDERABAD";
+	for(int i=0;i<=a.length()-1;i++)
+	{
+		if(i%2==0)
+		{
+			System.out.print(a.charAt(i));
+		}
+	}
+		
+
+
+
+
+
+}
+
+
+
+
+
+}
